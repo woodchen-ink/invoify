063c4be1b9416ae0b1a3618f5b6941e031f552d2
wood chen <wood@czl.net>
2024-12-10 23:54:19 +0800

    Integrate analytics script into RootLayout component
    - Added useEffect hook to dynamically load an analytics script from an external source.
    - Ensured the script is removed from the document head when the component unmounts for cleanup.
    - This change enhances tracking capabilities for user interactions on the site.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 import "@/app/globals.css";
 
 type Props = {
@@ -8,5 +8,19 @@ type Props = {
 // Since we have a `not-found.tsx` page on the root, a layout file
 // is required, even if it's just passing children through.
 export default function RootLayout({ children }: Props) {
+    useEffect(() => {
+        const script = document.createElement("script");
+        script.src = "https://analytics.czl.net/script.js";
+        script.defer = true;
+        script.dataset.websiteId = "39809dd9-8cbb-441a-b48f-3bcfa50cb1c4";
+        
+        document.head.appendChild(script);
+        
+        // Cleanup function to remove the script when component unmounts
+        return () => {
+            document.head.removeChild(script);
+        };
+    }, []);
+
     return children;
 }
