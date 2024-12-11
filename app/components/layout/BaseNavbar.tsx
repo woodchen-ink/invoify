58b7ee7a02a00965f6e5adea3d9cc41670fe3047
wood chen <wood@czl.net>
2024-12-11 15:45:17 +0800

    Update logo and navbar layout in BaseNavbar, and fix typo in SendPdfEmail template

--- app/components/layout/BaseNavbar.tsx
+++ app/components/layout/BaseNavbar.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 import Image from "next/image";
 
 // Assets
-const logoUrl = "https://i.czl.net/r2/2023/06/20/6491692758187.png";
+const logoUrl = "https://i.czl.net/r2/img/2024/12/675829b6933fb.png";
 
 // ShadCn
 import { Card } from "@/components/ui/card";
@@ -21,22 +21,30 @@ const BaseNavbar = () => {
     return (
         <header className="lg:container z-[99]">
             <nav>
-                <Card className="flex flex-wrap justify-between items-center px-5 gap-1">
-                    <Link href={"/"}>
-                        <img
-                            src={logoUrl}
-                            alt="CZL Express Logo"
-                            width={100}
-                            height={100}
-                            loading="eager"
-                        />
-                    </Link>
+                <Card className="flex flex-wrap justify-between items-center px-5 py-2 gap-1">
+                    <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+                        <Link href={"/"}>
+                            <img
+                                src={logoUrl}
+                                alt="CZL Express Logo"
+                                width={100}
+                                height={50}
+                                loading="eager"
+                            />
+                        </Link>
+                        <Link
+                            className="text-sm mt-2"
+                            href="https://exp.czl.net"
+                            target="_blank">进入官网</Link>
+                    </div>
                     <div className="flex flex-row items-center gap-2">
                         <h1 className="text-2xl font-bold">CZL Express 国际运输发票生成器</h1>
+
                     </div>
                     {/* ? DEV Only */}
                     {/* {devEnv && <DevDebug />} */}
                     <LanguageSelector />
+
                     <ThemeSwitcher />
                 </Card>
             </nav>
--- app/components/templates/email/SendPdfEmail.tsx
+++ app/components/templates/email/SendPdfEmail.tsx
@@ -21,7 +21,7 @@ type SendPdfEmailProps = {
 };
 
 export default function SendPdfEmail({ invoiceNumber }: SendPdfEmailProps) {
-    const logo = `https://i.czl.net/r2/2023/06/20/649169283f20c.png`;
+    const logo = `https://i.czl.net/r2/img/2024/12/675829b6933fb.png`;
     return (
         <Html>
             <Head />
@@ -34,12 +34,12 @@ export default function SendPdfEmail({ invoiceNumber }: SendPdfEmailProps) {
                         <Section className="bg-white border-black-950 my-10 px-10 py-4 rounded-md">
                             <Img
                                 src={logo}
-                                alt="Invoify Logo"
-                                width={200}
-                                height={120}
+                                alt="CZL Express Logo"
+                                width={100}
+                                height={80}
                             />
                             <Heading className="leading-tight">
-                                感谢您使用 Invoify!
+                                感谢您使用 CZL Express发票生成器!
                             </Heading>
 
                             <Text>
