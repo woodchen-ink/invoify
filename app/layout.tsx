9978f9f57fb1e7aed36e5ab805bea13b724252ab
wood chen <wood@czl.net>
2025-12-08 13:24:09 +0800

    feat: wrap children in RootLayout with additional Script component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from "react";
 import "@/app/globals.css";
+import Script from "next/script";
 
 type Props = {
     children: ReactNode;
@@ -8,5 +9,15 @@ type Props = {
 // Since we have a `not-found.tsx` page on the root, a layout file
 // is required, even if it's just passing children through.
 export default function RootLayout({ children }: Props) {
-    return children;
+    return (
+        <>
+            {children}
+            <Script
+                src="https://l.czl.net/script.js"
+                data-website-id="a3cfc446-c48f-4b28-a772-a8839c18cd25"
+                strategy="afterInteractive"
+                async
+            />
+        </>
+    );
 }
